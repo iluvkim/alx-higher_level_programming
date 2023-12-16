6c5aea517edc0e064a76cee3dc548f2052dc594c
iluvkim <kimbvundura@gmail.com>
2023-12-16 00:26:49 -0800

    1 and 2

--- 0x13-javascript_objects_scopes_closure/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closure/2-rectangle.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+
+class Rectangle {
+	constructor(w, h) {
+	  if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)) {
+	    this.width = w;
+	    this.height = h;
+    } else {
+	    this.width = 0;
+	    this.height = 0;
+    }
   }
-};
+}
